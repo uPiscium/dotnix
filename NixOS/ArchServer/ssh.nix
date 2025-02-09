1e429328787b62ce1adb01d26b8f167ae3a5c6c2
uPiscium <upiscium@gmail.com>
2025-02-10 07:52:05 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -6,14 +6,11 @@
       PubkeyAuthentication = "yes";
       PasswordAuthentication = false;
       PermitRootLogin = "no";
-      authorizedKeys = [
-        "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
-      ];
     };
   };
 
   users.users.upiscium.openssh.authorizedKeys.keys = [
-    "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
+    "ssh-ed25520 AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3 upiscium@ROGSTRIX"
   ];
 
   networking.firewall.allowedTCPPorts = [ 20000 ];
