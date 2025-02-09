bc38ea1647b17143574a85570248ada3277e3760
uPiscium <upiscium@gmail.com>
2025-02-09 23:07:21 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -6,12 +6,15 @@
       PubkeyAuthentication = "yes";
       PasswordAuthentication = false;
       PermitRootLogin = "no";
+      authorizedKeys = [
+        "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
+      ];
     };
   };
 
-  users.users.upiscium.openssh.authorizedKeys.keys = [
-    "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
-  ];
+  # users.users.upiscium.openssh.authorizedKeys.keys = [
+  #   "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
+  # ];
 
   networking.firewall.allowedTCPPorts = [ 20000 ];
 }
