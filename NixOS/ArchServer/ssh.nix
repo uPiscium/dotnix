7c85667ee314f9cd66fd9d340c67c599dbf44fca
uPiscium <upiscium@gmail.com>
2025-02-09 22:16:55 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -12,5 +12,10 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvyVdbmhxTZ4ui5c2ICJHN7RmmCpTjgWW0Zz+Py0dAo upiscium@ROGSTRIX"
   ];
 
+  networking.interfaces.enp5s0.ipv4.addresses = [ {
+    address = "192.168.11.130";
+    prefixLength = 24;
+  } ];
+
   networking.firewall.allowedTCPPorts = [ 20000 ];
 }
