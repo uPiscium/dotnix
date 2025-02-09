590cbf61f184c5f36cb37654ea6a4eb8d36dbf84
uPiscium <upiscium@gmail.com>
2025-02-10 08:19:14 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -1,22 +1,19 @@
 { ... }: {
   services.openssh = {
-    enable = true;
     ports = [ 20000 ];
-    # settings = {
-    #   PasswordAuthentication = false;
-    #   AllowUsers = [ "upiscium" ];
-    #   ChallengeResponseAuthentication = false;
-    #   PubkeyAuthentication = true;
-    #   PermitEmptyPasswords = false;
-    #   PermitRootLogin = "no";
-    #   KbdInteractiveAuthentication = false;
-    #   X11Forwarding = false;
-    # };
+    settings = {
+      PasswordAuthentication = false;
+      AllowUsers = [ "upiscium" ];
+      ChallengeResponseAuthentication = false;
+      PubkeyAuthentication = true;
+      PermitEmptyPasswords = false;
+      PermitRootLogin = "no";
+      KbdInteractiveAuthentication = false;
+      X11Forwarding = false;
+    };
   };
 
-  # users.users."upiscium".openssh.authorizedKeys.keys = [
-  #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmhk61jAkamnJnwiIOsiYf3EcPrfBn94QrAz8UL9pbs upiscium@ROGSTRIX"
-  # ];
-
-  networking.firewall.allowedTCPPorts = [ 20000 ];
+  users.users."upiscium".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmhk61jAkamnJnwiIOsiYf3EcPrfBn94QrAz8UL9pbs upiscium@ROGSTRIX"
+  ];
 }
--- NixOS/common/network.nix
+++ NixOS/common/network.nix
@@ -1,14 +1,14 @@
-{hostname, ...}: {
+{ hostname, ... }: {
   services.openssh.enable = true;
-  programs.kdeconnect.enable = true;
+  # programs.kdeconnect.enable = true;
 
   networking.hostName = hostname;
   networking.networkmanager.enable = true;
   networking.firewall = {
     enable = true;
-    trustedInterfaces = [];
-    allowedTCPPorts = [];
-    allowedUDPPorts = [];
+    trustedInterfaces = [ ];
+    allowedTCPPorts = [ 20000 ];
+    allowedUDPPorts = [ ];
     allowedTCPPortRanges = [
       # {from=0; to=0;}
     ];
