8906f18ff7c0dd173647e50a5e8b94539300ec32
uPiscium <upiscium@gmail.com>
2025-02-10 08:00:05 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -3,13 +3,14 @@
     enable = true;
     ports = [ 20000 ];
     settings = {
-      PubkeyAuthentication = "yes";
+      #PubkeyAuthentication = "yes";
       PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
     };
   };
 
-  users.users.upiscium.openssh.authorizedKeys.keys = [
+  users.users."upiscium".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmhk61jAkamnJnwiIOsiYf3EcPrfBn94QrAz8UL9pbs upiscium@ROGSTRIX"
   ];
 
