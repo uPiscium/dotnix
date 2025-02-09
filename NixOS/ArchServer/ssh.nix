e287e0ca9c7eaa7073e4204011de61138334c343
uPiscium <upiscium@gmail.com>
2025-02-09 22:45:31 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -9,7 +9,7 @@
   };
 
   users.users.upiscium.openssh.authorizedKeys.keys = [
-    "AAAAC3NzaC1lZDI1NTE5AAAAIJvyVdbmhxTZ4ui5c2ICJHN7RmmCpTjgWW0Zz+Py0dAo"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvyVdbmhxTZ4ui5c2ICJHN7RmmCpTjgWW0Zz+Py0dAo upiscium@ROGSTRIX"
   ];
 
   networking.firewall.allowedTCPPorts = [ 20000 ];
