87c71d4f37c63ea8ffd55b1a73cd1a29cb3920a0
uPiscium <upiscium@gmail.com>
2025-02-09 22:42:41 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -8,14 +8,9 @@
     };
   };
 
-  users.users.upiscium.openssh.authorizedKeys.keys = [
+  users.users."upiscium".openssh.authorizedKeys.keys = [
     "AAAAC3NzaC1lZDI1NTE5AAAAIJvyVdbmhxTZ4ui5c2ICJHN7RmmCpTjgWW0Zz+Py0dAo"
   ];
-
-  networking.interfaces.enp5s0.ipv4.addresses = [{
-    address = "192.168.11.130";
-    prefixLength = 24;
-  }];
 
   networking.firewall.allowedTCPPorts = [ 20000 ];
 }
