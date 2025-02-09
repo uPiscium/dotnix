0639406f8a3d7ccfa1fa74991a1b8a9b523f4ba2
uPiscium <upiscium@gmail.com>
2025-02-10 07:51:00 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -2,19 +2,19 @@
   services.openssh = {
     enable = true;
     ports = [ 20000 ];
-    # settings = {
-    #   PubkeyAuthentication = "yes";
-    #   PasswordAuthentication = false;
-    #   PermitRootLogin = "no";
-    #   authorizedKeys = [
-    #     "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
-    #   ];
-    # };
+    settings = {
+      PubkeyAuthentication = "yes";
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+      authorizedKeys = [
+        "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
+      ];
+    };
   };
 
-  # users.users.upiscium.openssh.authorizedKeys.keys = [
-  #   "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
-  # ];
+  users.users.upiscium.openssh.authorizedKeys.keys = [
+    "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
+  ];
 
   networking.firewall.allowedTCPPorts = [ 20000 ];
 }
