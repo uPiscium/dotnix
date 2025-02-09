be98561198f44da93eb9ba994a09105290e5c2a4
uPiscium <upiscium@gmail.com>
2025-02-10 07:47:39 +0900

    update

--- NixOS/ArchServer/ssh.nix
+++ NixOS/ArchServer/ssh.nix
@@ -2,14 +2,14 @@
   services.openssh = {
     enable = true;
     ports = [ 20000 ];
-    settings = {
-      PubkeyAuthentication = "yes";
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-      authorizedKeys = [
-        "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
-      ];
-    };
+    # settings = {
+    #   PubkeyAuthentication = "yes";
+    #   PasswordAuthentication = false;
+    #   PermitRootLogin = "no";
+    #   authorizedKeys = [
+    #     "AAAAC3NzaC1lZDI1NTE5AAAAIEIhW/ts3oIwZVt9LtK9M9ksTqTAW0M8oZVIby8+w/z3"
+    #   ];
+    # };
   };
 
   # users.users.upiscium.openssh.authorizedKeys.keys = [
