ed435b6768fd8b1dac15a80ef71b35ec72930356
uPiscium <upiscium@gmail.com>
2025-02-10 23:00:31 +0900

    update

--- NixOS/NixServer/ssh.nix
+++ NixOS/NixServer/ssh.nix
@@ -1,7 +1,7 @@
 { ... }: {
-  users.users.upiscium.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfOfHIAEsrWG0hdPDB+TPXoDJ6qUffe0Z8xOPgSUV8e upiscium"
-  ];
+  # users.users.upiscium.openssh.authorizedKeys.keys = [
+  #   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfOfHIAEsrWG0hdPDB+TPXoDJ6qUffe0Z8xOPgSUV8e upiscium"
+  # ];
   services.openssh = {
     ports = [ 20000 ];
     settings = {
@@ -13,10 +13,10 @@
     };
   };
 
-  programs.ssh = {
-    pubkeyAcceptedKeyTypes = [ "ssh-ed25519" ];
-    hostKeyAlgorithms = [ "ssh-ed25519" ];
-  };
+  # programs.ssh = {
+  #   pubkeyAcceptedKeyTypes = [ "ssh-ed25519" ];
+  #   hostKeyAlgorithms = [ "ssh-ed25519" ];
+  # };
 
   networking.firewall.allowedTCPPorts = [ 20000 ];
   networking.firewall.allowedUDPPorts = [ 20000 ];
