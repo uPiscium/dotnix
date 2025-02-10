801630d41cd73c8dbe8b9a659b9440970e788352
uPiscium <upiscium@gmail.com>
2025-02-10 23:52:42 +0900

    update

---
+++ NixOS/MinecraftServer/ssh.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  services.openssh = {
+    enable = true;
+    ports = [ 20000 ];
+    passwordAuthentication = false;
+    permitRootLogin = "no";
+  };
+
+  users.users.upiscium.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCp49Fwz/03KWdH63I3AFz1vIu9BJyew+NcP4TSoua1 upiscium"
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 20000 ];
+}
