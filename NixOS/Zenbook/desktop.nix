3a3dadecce23f10a797e29e05a1c0d8645421861
uPiscium <upiscium@gmail.com>
2025-03-20 12:13:12 +0900

    update

---
+++ NixOS/Zenbook/desktop.nix
@@ -0,0 +1,17 @@
+{inputs, pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    sddm-chili-theme
+  ];
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
+
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "chili";
+    wayland.enable = true;
+  };
+}
---
+++ NixOS/Zenbook/wireguard.nix
@@ -0,0 +1,23 @@
+{ ... }: {
+  networking.firewall = {
+    allowedUDPPorts = [ 30000 ];
+  };
+
+  networking.wireguard.enable = true;
+  networking.wireguard.interfaces = {
+    wg0 = {
+      ips = [ "10.0.0.2/24" ];
+      listenPort = 30000;
+      mtu = 1420;
+      privateKeyFile = "/home/upiscium/WireGuard/ROGSTRIX.key";
+      peers = [
+        {
+          publicKey = "rj2Fy53+fondOM1dZ9trbNaXAVEM438UAcC6ni6beQM=";
+          allowedIPs = [ "10.0.0.0/24" "192.168.11.0/24" ];
+          endpoint = "uPiscium.f5.si:30000";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+}
