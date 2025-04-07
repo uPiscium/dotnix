6d798197feac88196c974be5ae3defa5f646b8cc
uPiscium <upiscium@gmail.com>
2025-04-07 14:35:08 +0900

    update

--- config.d/nvim/lua/plugins/lazyLoad/alpha.lua
+++ config.d/nvim/lua/plugins/lazyLoad/alpha.lua
@@ -52,34 +52,44 @@ return {
 			[[       .;oc        'coo;.      @@#.                 .:+%@@@@@@@@@@@%.%@@@@@@@@@@@@+:.                                                    ]],
 			[[         .'         .,.        %:                         :=*%%@@@@%.%@@@@%%*=:                                                          ]],
 		}
+		local headerAscii = {
+			[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+			[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+			[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+			[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+			[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+			[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+		}
 		local api = require("image")
 		local image = api.from_file(vim.fn.expand("$HOME/dotnix/config.d/nvim/Logo.png"), {
-			x = math.floor(vim.api.nvim_win_get_width(0) / 2) - 50,
+			x = math.floor(vim.api.nvim_win_get_width(0) / 2) - 28,
 			y = 10,
 			width = 100,
 		})
 
-		dashboard.section.header.val = {}
+		dashboard.section.header.val = headerAscii
 		dashboard.section.header.opts.hl = "CustomAlphaHeader"
 		dashboard.section.buttons.val = {}
 		dashboard.section.footer.opts.hl = "CustomAlphaFooter"
 		dashboard.config.opts.noautocmd = true
 
 		if image ~= nil then
-			dashboard.section.header.val = asciiArt
-		else
+			-- dashboard.section.header.val = asciiArt
 			vim.api.nvim_create_autocmd({ "User" }, {
 				callback = function()
 					image:render()
+          -- dashboard.section.header.val = {}
 				end,
 				pattern = "AlphaReady",
 			})
 			vim.api.nvim_create_autocmd({ "BufEnter" }, {
 				callback = function()
 					image:clear()
+          -- dashboard.section.header.val = asciiArt
 				end,
 				pattern = { "*" },
 			})
+		else
 		end
 
 		vim.cmd([[
