67f7a3cff98c773906e8b86b4035fed3a2428306
uPiscium <upiscium@gmail.com>
2025-04-24 10:36:23 +0900

    update

---
+++ NixOS/ROGSTRIX/grub.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  boot.loader = {
+    systemd-boot.enable = false;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    grub = {
+      devices = [ "nodev" ];
+      enable = true;
+      efiSupport = true;
+      version = 2;
+      useOSProber = true;
+    };
+  };
+}
--- NixOS/ROGSTRIX/host.nix
+++ NixOS/ROGSTRIX/host.nix
@@ -12,6 +12,7 @@
       ../common
       ./asusctl.nix
       ./desktop.nix
+      ./grub.nix
       ./network.nix
       ./steam.nix
       ./wireguard.nix
@@ -50,36 +51,22 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader = {
-    systemd-boot.enable = false;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
-    };
-    grub = {
-      devices = [ "nodev" ];
-      enable = true;
-      efiSupport = true;
-      version = 2;
-      useOSProber = true;
-    };
-      };
 
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-    environment.systemPackages = with pkgs; [
-      nvtopPackages.full
-      wireguard-tools
-    ];
+  environment.systemPackages = with pkgs; [
+    nvtopPackages.full
+    wireguard-tools
+  ];
 
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.11"; # Did you read the comment?
-  }
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
 
