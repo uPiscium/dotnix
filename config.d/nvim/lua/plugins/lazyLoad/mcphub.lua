2493bdd77b345cd5c4ab1a40b91003c40db39413
uPiscium <upiscium@gmail.com>
2025-05-02 14:08:23 +0900

    update

--- config.d/nvim/lua/plugins/lazyLoad/mcphub.lua
+++ config.d/nvim/lua/plugins/lazyLoad/mcphub.lua
@@ -5,12 +5,13 @@ return {
 	},
 	-- comment the following line to ensure hub will be ready at the earliest
 	cmd = "MCPHub", -- lazy load by default
-	build = "bun install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+  build = 'bundled_build.lua',
 	-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
 	-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
 	config = function()
 		require("mcphub").setup({
       auto_approve = false,
+      use_bundled_binary = true,
     })
 	end,
 }
