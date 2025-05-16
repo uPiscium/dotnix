7c94b6e85cc912f378b627c5d5c5545be3912bb2
uPiscium <upiscium@gmail.com>
2025-05-16 18:33:36 +0900

    update

--- NixOS/NetworkServer/ipfix.nix
+++ NixOS/NetworkServer/ipfix.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   networking.useDHCP = false;
   networking.interfaces.eno1 = {
     useDHCP = false;
@@ -10,5 +10,5 @@
     ];
   };
   networking.defaultGateway = "192.168.11.1";
-  networking.nameservers = ["8.8.8.8"];
+  networking.nameservers = [ "127.0.0.1" ];
 }
