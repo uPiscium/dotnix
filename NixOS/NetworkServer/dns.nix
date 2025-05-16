403ca1dac684e54759d8ba1760845187ce535671
uPiscium <upiscium@gmail.com>
2025-05-16 22:19:29 +0900

    update

--- NixOS/NetworkServer/dns.nix
+++ NixOS/NetworkServer/dns.nix
@@ -18,6 +18,7 @@
       "/desktop.iruma/192.168.11.90"
       "/network.iruma/192.168.11.100"
       "/nas.iruma/192.168.11.120"
+      "/nextcloud.iruma/192.168.11.120:40000"
       "/minecraft.iruma/192.168.11.130"
     ];
   };
