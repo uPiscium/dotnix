776461733297b401d0553c7dc80a30175a954d1e
uPiscium <upiscium@gmail.com>
2025-05-16 11:01:44 +0900

    update

--- NixOS/NetworkServer/unbound.nix
+++ NixOS/NetworkServer/unbound.nix
@@ -1,6 +1,6 @@
 { ... }: {
   networking.firewall.allowedTCPPorts = [ 5335 ];
-  networking.firewall.allowedUCPPorts = [ 5335 ];
+  networking.firewall.allowedUDPPorts = [ 5335 ];
 
   services.unbound = {
     enable = true;
@@ -17,9 +17,9 @@
         ];
 
         # Based on recommended settings in https://docs.pi-hole.net/guides/dns/unbound/#configure-unbound
-        do-ip4 = true;
-        do-udp = true;
-        do-tcp = true;
+        # do-ip4 = true;
+        # do-udp = true;
+        # do-tcp = true;
         harden-glue = true;
         harden-dnssec-stripped = true;
         use-caps-for-id = false;
@@ -33,12 +33,11 @@
           "192.168.11.0/24"
           "10.0.0.0/24"
         ];
-
         local-data = [
-          "iruma.local.desktop 3600 IN A 192.168.11.90"
-          "iruma.local.network 3600 IN A 192.168.11.100"
-          "iruma.local.nas 3600 IN A 192.168.11.120"
-          "iruma.local.minecraftserver. 3600 IN A 192.168.11.130"
+          ''"iruma.local.desktop            3600 IN A 192.168.11.90"''
+          ''"iruma.local.network            3600 IN A 192.168.11.100"''
+          ''"iruma.local.nas                3600 IN A 192.168.11.120"''
+          ''"iruma.local.minecraftserver.   3600 IN A 192.168.11.130"''
         ];
       };
       forward-zone = [
