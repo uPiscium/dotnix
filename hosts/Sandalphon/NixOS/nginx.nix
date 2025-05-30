9c892ba9e53713ff3222737ec5e0a86879be5ddc
uPiscium <upiscium@gmail.com>
2025-05-31 00:46:39 +0900

    update

--- hosts/Sandalphon/NixOS/nginx.nix
+++ hosts/Sandalphon/NixOS/nginx.nix
@@ -1,11 +1,32 @@
 { ... }: {
   services.nginx = {
     enable = true;
-    virtualHosts."qwen3.iruma.arc" = {
+    virtualHosts."ollama.iruma.arc" = {
       forceSSL = false; # allow HTTP access
       locations."/" = {
         proxyPass = "http://192.168.11.90:3000";
         proxyWebsockets = true; # allow WebSocket
+      };
+    };
+    virtualHosts."gabriel.iruma.arc" = {
+      forceSSL = false;
+      locations."/" = {
+        proxyPass = "http://192.168.11.80:8006";
+        proxyWebsockets = true;
+      };
+    };
+    virtualHosts."metatron.iruma.arc" = {
+      forceSSL = false;
+      locations."/" = {
+        proxyPass = "http://192.168.11.82:8006";
+        proxyWebsockets = true;
+      };
+    };
+    virtualHosts."raphael.iruma.arc" = {
+      forceSSL = false;
+      locations."/" = {
+        proxyPass = "http://192.168.11.81:8006";
+        proxyWebsockets = true;
       };
     };
   };
