6b788d48943a9e2b78501b2823085a4eac767581
uPiscium <upiscium@gmail.com>
2025-05-30 21:14:42 +0900

    update

--- hosts/Ramiel/NixOS/nginx.nix
+++ hosts/Ramiel/NixOS/nginx.nix
@@ -1,3 +1,15 @@
-{ ... } : {
-  
+{ ... }: {
+  services.nginx = {
+    enable = true;
+    virtualHosts."qwen3.iruma.arc" = {
+      # HTTP のみ（HTTPSが不要な場合）
+      forceSSL = false;
+      locations."/" = {
+        proxyPass = "http://desktop.iruma.arc:3000";
+        proxyWebsockets = true; # WebSocketも通す場合
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 ];
 }
