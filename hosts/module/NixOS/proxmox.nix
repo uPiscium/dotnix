d4d3f8d3a31373dab98c9f5116237ef6b92dd995
uPiscium <upiscium@gmail.com>
2025-05-30 14:07:31 +0900

    update

--- hosts/module/NixOS/proxmox.nix
+++ hosts/module/NixOS/proxmox.nix
@@ -1,5 +1,5 @@
 { ... }: {
-  networking.hostName = "base"; # Provide a default hostname
+  # networking.hostName = "base"; # Provide a default hostname
   services.qemuGuest.enable = true; # Enable QEMU Guest for Proxmox
   boot.loader.grub.enable = true; # Use the boot drive for GRUB
   boot.loader.grub.devices = [ "nodev" ];
