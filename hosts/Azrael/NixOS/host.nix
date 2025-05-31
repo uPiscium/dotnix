843a5efd394463fa0241948024bb7beb46be53e2
uPiscium <upiscium@gmail.com>
2025-05-31 23:45:43 +0900

    update

--- hosts/Azrael/NixOS/host.nix
+++ hosts/Azrael/NixOS/host.nix
@@ -36,14 +36,14 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      # intelBusId = "PCI:1:0:0";
-      nvidiaBusId = "PCI:0:2:0";
-    };
+    # prime = {
+    #   offload = {
+    #     enable = true;
+    #     enableOffloadCmd = true;
+    #   };
+    #   # intelBusId = "PCI:1:0:0";
+    #   nvidiaBusId = "PCI:0:2:0";
+    # };
   };
   # Bootloader.
   # boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
