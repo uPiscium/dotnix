71b0b3d47a76b20d4be64dad43e135a2185b8dec
uPiscium <upiscium@gmail.com>
2025-06-01 20:31:22 +0900

    update

--- hosts/Azrael/NixOS/host.nix
+++ hosts/Azrael/NixOS/host.nix
@@ -18,15 +18,14 @@
     ]
     ++ (with inputs.nixos-hardware.nixosModules; [
       common-cpu-amd
+      # common-gpu-nvidia
       common-pc-ssd
     ]);
 
   services.xserver.videoDrivers = [ "nvidia" ];
-
   hardware.graphics = {
     enable = true;
   };
-
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
@@ -34,14 +33,9 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-    # prime = {
-    #   offload = {
-    #     enable = true;
-    #     enableOffloadCmd = true;
-    #   };
-    #   intelBusId = "PCI:1:0:0";
-    #   nvidiaBusId = "PCI:0:2:0";
-    # };
+    prime = {
+      nvidiaBusId = "PCI:0:10:0";
+    };
   };
   # Bootloader.
   # boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
