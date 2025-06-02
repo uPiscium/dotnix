cb9c34de9d2f6bdb2ccb0894c4fb63b7edfa9a04
uPiscium <upiscium@gmail.com>
2025-06-02 22:22:19 +0900

    update

--- hosts/Israfel/NixOS/nas.nix
+++ hosts/Israfel/NixOS/nas.nix
@@ -1,7 +1,40 @@
 { pkgs, ... }: {
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.enableUnstable = false; # Unstable ZFS features are not enabled
+  services.zfs.autoScrub.enable = true; # Enable automatic scrubbing of ZFS pools
+
   environment.systemPackages = with pkgs; [
     zfs
   ];
+
+  fileSystems."/mnt/NAS/" = {
+    device = "tank/data";
+    fsType = "zfs";
+  };
+
+  services.samba = {
+    enable = true;
+    workgroup = "WORKGROUP";
+    shares = {
+      data = {
+        path = "/mnt/NAS/";
+        readOnly = false;
+        browseable = true;
+        "guest ok" = false;
+      };
+    };
+  };
+
+  services.nfs.server = {
+    enable = true;
+    exports = [
+      {
+        path = "/mnt/NAS/";
+        clients = [ "*" ];
+        options = [ "rw" "no_root_squash" ];
+      }
+    ];
+  };
   # services.nas = {
   #   enable = true;
   #   packages = with pkgs; [
