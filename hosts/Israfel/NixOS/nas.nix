8f4f5b74729dafa25234c765c184513efbf45acb
uPiscium <upiscium@gmail.com>
2025-06-02 22:32:57 +0900

    update

--- hosts/Israfel/NixOS/nas.nix
+++ hosts/Israfel/NixOS/nas.nix
@@ -1,10 +1,33 @@
 { pkgs, ... }: {
   boot.supportedFilesystems = [ "zfs" ];
-  services.zfs.autoScrub.enable = true; # Enable automatic scrubbing of ZFS pools
+  boot.zfs = {
+    enabled = true; # Enable ZFS support
+    forceImportRoot = false; # Set to true if you want to force import the root pool
+    forceImportAll = true; # Set to true if you want to force import all ZFS pools
+  };
 
   environment.systemPackages = with pkgs; [
     zfs
   ];
+
+  services.zfs = {
+    enable = true; # Enable ZFS services
+    autoScrub.enable = true; # Enable automatic scrubbing of ZFS pools
+    autoSnapshot = {
+      enable = true; # Enable automatic snapshots
+      frequent = 4; # Number of frequent snapshots to keep /15 minutes
+      hourly = 24; # Number of hourly snapshots to keep /1 hour
+      daily = 7; # Number of daily snapshots to keep /1 day
+      weekly = 4; # Number of weekly snapshots to keep /1 week
+      monthly = 3; # Number of monthly snapshots to keep /1 month
+    };
+    pools = {
+      tank = {
+        devices = []; # Replace with your actual disk ID
+        autoScrub.enable = true; # Enable automatic scrubbing for this pool
+      };
+    };
+  };
 
   # fileSystems."/mnt/NAS/" = {
   #   device = "tank/data";
