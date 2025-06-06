5f99fcb9273c1bd4582758bc1397f2d90c3d1065
uPiscium <upiscium@gmail.com>
2025-06-06 11:45:59 +0900

    update

--- hosts/Sahaquiel/NixOS/gitlab.nix
+++ hosts/Sahaquiel/NixOS/gitlab.nix
@@ -1,33 +1,19 @@
 { ... }: {
   services.gitlab = {
     enable = true;
-    externalUrl = "https://sahaquiel.iruma.arc";
+    host = "sahaquiel.iruma.arc";
+    port = 3000;
     initialRootPasswordFile = "/var/keys/gitlab-root-password";
-    smtp = {
-      enable = true;
-      address = "sahaquiel.iruma.arc";
-      port = 587;
-      user = "upiscium@gmail.com";
-      passwordFile = "/var/keys/gitlab-smtp-password";
-      domain = "iruma.arc";
-      authentication = "login";
-      startTLS = true;
-    };
     databasePasswordFile = "/var/keys/gitlab-db-password";
+    # smtp = {
+    #   enable = true;
+    #   address = "sahaquiel.iruma.arc";
+    #   port = 587;
+    #   user = "upiscium@gmail.com";
+    #   passwordFile = "/var/keys/gitlab-smtp-password";
+    #   domain = "iruma.arc";
+    #   authentication = "login";
+    #   startTLS = true;
+    # };
   };
-
-  services.nginx = {
-    enable = true;
-    virtualHosts."gitlab.example.com" = {
-      enableACME = true;
-      forceSSL = true;
-    };
-  };
-
-  security.acme = {
-    acceptTerms = true;
-    email = "upiscium@gmail.com";
-  };
-
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
