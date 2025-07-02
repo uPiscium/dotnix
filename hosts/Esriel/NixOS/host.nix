824fe2bd84b9c55638cc6f9d868b985fd202fe77
uPiscium <upiscium@gmail.com>
2025-07-02 13:43:37 +0900

    update

--- hosts/Esriel/NixOS/host.nix
+++ hosts/Esriel/NixOS/host.nix
@@ -10,58 +10,28 @@
       # Include the results of the hardware scan.
       ./firewall.nix
       ./hardware.nix
+      ./ipfix.nix
 
       ../../common/NixOS
-      ../../module/NixOS/network.nix
       ../../module/NixOS/proxmox.nix
+      ../../module/NixOS/ssh.nix
     ]
     ++ (with inputs.nixos-hardware.nixosModules; [
-      common-cpu-intel
-      common-gpu-nvidia
+      common-cpu-amd
       common-pc-ssd
     ]);
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      intelBusId = "PCI:1:0:0";
-      nvidiaBusId = "PCI:0:2:0";
-    };
-  };
-
-  # hardware.nvidia-container-toolkit.enable = true;
-
   # Bootloader.
   # boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
-  # boot.kernelPackages = pkgs.linuxPackages_6_1; # NVIDIA driver is not compatible with 6.6 yet
-  boot.kernelPackages = pkgs.linuxPackages_latest; # Use latest kernel for better hardware support
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  services.xserver.displayManager.lightdm.enable = false;
 
-  environment.systemPackages = with pkgs; [
-    nvtopPackages.full
-    wireguard-tools
-  ];
-
+  # environment.systemPackages = with pkgs; [ ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -71,4 +41,3 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 }
-
