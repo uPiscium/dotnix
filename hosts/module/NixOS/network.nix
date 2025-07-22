3d673bb36b10ece3c3e87f41b291481cd8d70580
uPiscium <upiscium@gmail.com>
2025-07-22 21:03:42 +0900

    Initial commit

--- hosts/module/NixOS/network.nix
+++ hosts/module/NixOS/network.nix
@@ -1,10 +1,10 @@
 { ... }: {
-  environment.etc."resolv.conf".text = ''
-    # SET BY NIX CONFIG
-    search ictv.ne.jp
-    nameserver 10.0.0.5
-    nameserver 8.8.8.8
-    options edns0
-  '';
+  # environment.etc."resolv.conf".text = ''
+  #   # SET BY NIX CONFIG
+  #   search ictv.ne.jp
+  #   nameserver 10.0.0.5
+  #   nameserver 8.8.8.8
+  #   options edns0
+  # '';
 }
 
