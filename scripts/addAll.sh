909fa30f9fdabf89b19bf426f4785473645dadde
uPiscium <upiscium@gmail.com>
2025-07-28 17:49:55 +0900

    update

--- scripts/addAll.sh
+++ scripts/addAll.sh
@@ -3,14 +3,22 @@
 echo -e "\033[1;32mAdd all files to git repository.\033[0m";
 echo -e "\033[1;32mChecking for over 100M files.\033[0m";
 set files=$(find . -size +100M);
+
 if [ ! -f .gitignore ]; then
   echo -e "\033[1;31mNo .gitignore file found.\033[0m";
-  exit 0;
+  echo -ne "\033[1;32mProceed? (y/N)\033[0m";
+  read answer;
+  if [ "$answer" != "y" ]; then
+    echo -ne "\033[1;32mSkip .gitignore file.\033[0m";
+  else
+    exit 1;
+  fi
+else
+  cat .gitignore | grep -v '^#' | grep -v '^$' | while read line; do
+    set files=$(echo "$files" | grep -v "$line");
+  done
 fi
 
-cat .gitignore | grep -v '^#' | grep -v '^$' | while read line; do
-  set files=$(echo "$files" | grep -v "$line");
-done
 if [ -n "$files" ]; then
   echo -e "\033[1;31mOver 100M files detected.\033[0m";
   for file in $files; do
