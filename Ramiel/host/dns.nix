72ef487a95afe3ac421064e625197723d32bbd4d
uPiscium <upiscium@gmail.com>
2025-08-31 11:11:18 +0900

    update

--- Ramiel/host/dns.nix
+++ Ramiel/host/dns.nix
@@ -58,9 +58,9 @@
   #   ];
   # };
 
-  services.resolved = lib.mkForce {
-    enable = false;
-  };
+  # services.resolved = lib.mkForce {
+  #   enable = false;
+  # };
 
   services.unbound = {
     enable = true;
@@ -83,52 +83,52 @@
         harden-glue = true;
         harden-dnssec-stripped = true;
 
-      local-data = [
-        # Proxmox webUI endpoint
-        "\"proxmox.iruma.arc. IN A 192.168.11.114\""
-        "\"proxmox-gabriel.iruma.arc. IN A 192.168.11.114\""
-        "\"proxmox-metatron.iruma.arc. IN A 192.168.11.114\""
-        "\"proxmox-raphael.iruma.arc. IN A 192.168.11.114\""
-        "\"proxmox-zadkiel.iruma.arc. IN A 192.168.11.114\""
+        local-data = [
+          # Proxmox webUI endpoint
+          "\"proxmox.iruma.arc. IN A 192.168.11.114\""
+          "\"proxmox-gabriel.iruma.arc. IN A 192.168.11.114\""
+          "\"proxmox-metatron.iruma.arc. IN A 192.168.11.114\""
+          "\"proxmox-raphael.iruma.arc. IN A 192.168.11.114\""
+          "\"proxmox-zadkiel.iruma.arc. IN A 192.168.11.114\""
 
-        # Proxmox servers
-        "\"gabriel.iruma.arc. IN A 192.168.11.80\""
-        "\"metatron.iruma.arc. IN A 192.168.11.82\""
-        "\"raphael.iruma.arc. IN A 192.168.11.81\""
-        "\"zadkiel.iruma.arc. IN A 192.168.11.83\""
+          # Proxmox servers
+          "\"gabriel.iruma.arc. IN A 192.168.11.80\""
+          "\"metatron.iruma.arc. IN A 192.168.11.82\""
+          "\"raphael.iruma.arc. IN A 192.168.11.81\""
+          "\"zadkiel.iruma.arc. IN A 192.168.11.83\""
 
-        # Gabriel VMs
-        "\"azrael.iruma.arc. IN A 192.168.11.100\""
-        "\"ariel.iruma.arc. IN A 192.168.11.102\""
+          # Gabriel VMs
+          "\"azrael.iruma.arc. IN A 192.168.11.100\""
+          "\"ariel.iruma.arc. IN A 192.168.11.102\""
 
-        # Metatron VMs
-        "\"uriel.iruma.arc. IN A 192.168.11.112\""
-        "\"ramiel.iruma.arc. IN A 192.168.11.113\""
-        "\"sandalphon.iruma.arc. IN A 192.168.11.114\""
+          # Metatron VMs
+          "\"uriel.iruma.arc. IN A 192.168.11.112\""
+          "\"ramiel.iruma.arc. IN A 192.168.11.113\""
+          "\"sandalphon.iruma.arc. IN A 192.168.11.114\""
 
-        # Raphael VMs
-        "\"israfel.iruma.arc. IN A 192.168.11.128\""
-        "\"nathaniel.iruma.arc. IN A 192.168.11.129\""
-        "\"haniel.iruma.arc. IN A 192.168.11.130\""
+          # Raphael VMs
+          "\"israfel.iruma.arc. IN A 192.168.11.128\""
+          "\"nathaniel.iruma.arc. IN A 192.168.11.129\""
+          "\"haniel.iruma.arc. IN A 192.168.11.130\""
 
-        # Zadkiel VMs
-        "\"baraqiel.iruma.arc. IN A 192.168.11.144\""
+          # Zadkiel VMs
+          "\"baraqiel.iruma.arc. IN A 192.168.11.144\""
 
-        # Services
-        "\"ollama.iruma.arc. IN A 192.168.11.114\""
+          # Services
+          "\"ollama.iruma.arc. IN A 192.168.11.114\""
 
-        # Aliases
-        "\"desktop.iruma.arc. IN A 192.168.11.90\""
-        "\"gpu-1.iruma.arc. IN A 192.168.11.100\""
-        "\"minecraft.iruma.arc. IN A 192.168.11.102\""
-        "\"wireguard.iruma.arc. IN A 192.168.11.112\""
-        "\"dns.iruma.arc. IN A 192.168.11.113\""
-        "\"nginx.iruma.arc. IN A 192.168.11.114\""
-        "\"nextcloud.iruma.arc. IN A 192.168.11.128\""
-        "\"nas.iruma.arc. IN A 192.168.11.128\""
-        "\"apache.iruma.arc. IN A 192.168.11.129\""
-        "\"container-runnner.iruma.arc. IN A 192.168.11.130\""
-      ];
+          # Aliases
+          "\"desktop.iruma.arc. IN A 192.168.11.90\""
+          "\"gpu-1.iruma.arc. IN A 192.168.11.100\""
+          "\"minecraft.iruma.arc. IN A 192.168.11.102\""
+          "\"wireguard.iruma.arc. IN A 192.168.11.112\""
+          "\"dns.iruma.arc. IN A 192.168.11.113\""
+          "\"nginx.iruma.arc. IN A 192.168.11.114\""
+          "\"nextcloud.iruma.arc. IN A 192.168.11.128\""
+          "\"nas.iruma.arc. IN A 192.168.11.128\""
+          "\"apache.iruma.arc. IN A 192.168.11.129\""
+          "\"container-runnner.iruma.arc. IN A 192.168.11.130\""
+        ];
       };
 
       forward-zone = {
