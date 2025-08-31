0ead80e3e7de238dfad59217e53f23b399964ecd
uPiscium <upiscium@gmail.com>
2025-08-31 11:10:50 +0900

    update

--- Ramiel/host/dns.nix
+++ Ramiel/host/dns.nix
@@ -82,6 +82,53 @@
         hide-version = true;
         harden-glue = true;
         harden-dnssec-stripped = true;
+
+      local-data = [
+        # Proxmox webUI endpoint
+        "\"proxmox.iruma.arc. IN A 192.168.11.114\""
+        "\"proxmox-gabriel.iruma.arc. IN A 192.168.11.114\""
+        "\"proxmox-metatron.iruma.arc. IN A 192.168.11.114\""
+        "\"proxmox-raphael.iruma.arc. IN A 192.168.11.114\""
+        "\"proxmox-zadkiel.iruma.arc. IN A 192.168.11.114\""
+
+        # Proxmox servers
+        "\"gabriel.iruma.arc. IN A 192.168.11.80\""
+        "\"metatron.iruma.arc. IN A 192.168.11.82\""
+        "\"raphael.iruma.arc. IN A 192.168.11.81\""
+        "\"zadkiel.iruma.arc. IN A 192.168.11.83\""
+
+        # Gabriel VMs
+        "\"azrael.iruma.arc. IN A 192.168.11.100\""
+        "\"ariel.iruma.arc. IN A 192.168.11.102\""
+
+        # Metatron VMs
+        "\"uriel.iruma.arc. IN A 192.168.11.112\""
+        "\"ramiel.iruma.arc. IN A 192.168.11.113\""
+        "\"sandalphon.iruma.arc. IN A 192.168.11.114\""
+
+        # Raphael VMs
+        "\"israfel.iruma.arc. IN A 192.168.11.128\""
+        "\"nathaniel.iruma.arc. IN A 192.168.11.129\""
+        "\"haniel.iruma.arc. IN A 192.168.11.130\""
+
+        # Zadkiel VMs
+        "\"baraqiel.iruma.arc. IN A 192.168.11.144\""
+
+        # Services
+        "\"ollama.iruma.arc. IN A 192.168.11.114\""
+
+        # Aliases
+        "\"desktop.iruma.arc. IN A 192.168.11.90\""
+        "\"gpu-1.iruma.arc. IN A 192.168.11.100\""
+        "\"minecraft.iruma.arc. IN A 192.168.11.102\""
+        "\"wireguard.iruma.arc. IN A 192.168.11.112\""
+        "\"dns.iruma.arc. IN A 192.168.11.113\""
+        "\"nginx.iruma.arc. IN A 192.168.11.114\""
+        "\"nextcloud.iruma.arc. IN A 192.168.11.128\""
+        "\"nas.iruma.arc. IN A 192.168.11.128\""
+        "\"apache.iruma.arc. IN A 192.168.11.129\""
+        "\"container-runnner.iruma.arc. IN A 192.168.11.130\""
+      ];
       };
 
       forward-zone = {
@@ -95,52 +142,6 @@
 
       local-zone = [
         { name = "iruma.arc."; type = "static"; }
-      ];
-      local-data = [
-        # Proxmox webUI endpoint
-        "proxmox.iruma.arc. IN A 192.168.11.114"
-        "proxmox-gabriel.iruma.arc. IN A 192.168.11.114"
-        "proxmox-metatron.iruma.arc. IN A 192.168.11.114"
-        "proxmox-raphael.iruma.arc. IN A 192.168.11.114"
-        "proxmox-zadkiel.iruma.arc. IN A 192.168.11.114"
-
-        # Proxmox servers
-        "gabriel.iruma.arc. IN A 192.168.11.80"
-        "metatron.iruma.arc. IN A 192.168.11.82"
-        "raphael.iruma.arc. IN A 192.168.11.81"
-        "zadkiel.iruma.arc. IN A 192.168.11.83"
-
-        # Gabriel VMs
-        "azrael.iruma.arc. IN A 192.168.11.100"
-        "ariel.iruma.arc. IN A 192.168.11.102"
-
-        # Metatron VMs
-        "uriel.iruma.arc. IN A 192.168.11.112"
-        "ramiel.iruma.arc. IN A 192.168.11.113"
-        "sandalphon.iruma.arc. IN A 192.168.11.114"
-
-        # Raphael VMs
-        "israfel.iruma.arc. IN A 192.168.11.128"
-        "nathaniel.iruma.arc. IN A 192.168.11.129"
-        "haniel.iruma.arc. IN A 192.168.11.130"
-
-        # Zadkiel VMs
-        "baraqiel.iruma.arc. IN A 192.168.11.144"
-
-        # Services
-        "ollama.iruma.arc. IN A 192.168.11.114"
-
-        # Aliases
-        "desktop.iruma.arc. IN A 192.168.11.90"
-        "gpu-1.iruma.arc. IN A 192.168.11.100"
-        "minecraft.iruma.arc. IN A 192.168.11.102"
-        "wireguard.iruma.arc. IN A 192.168.11.112"
-        "dns.iruma.arc. IN A 192.168.11.113"
-        "nginx.iruma.arc. IN A 192.168.11.114"
-        "nextcloud.iruma.arc. IN A 192.168.11.128"
-        "nas.iruma.arc. IN A 192.168.11.128"
-        "apache.iruma.arc. IN A 192.168.11.129"
-        "container-runnner.iruma.arc. IN A 192.168.11.130"
       ];
     };
   };
