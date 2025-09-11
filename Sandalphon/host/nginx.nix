0986982e361f947dac1d185b7e469825d3f969f5
uPiscium <upiscium@gmail.com>
2025-09-11 14:23:24 +0900

    update

--- Sandalphon/host/nginx.nix
+++ Sandalphon/host/nginx.nix
@@ -6,11 +6,9 @@
     recommendedOptimisation = true;
 
     virtualHosts."ollama.iruma.arc" = {
-      forceSSL = true;
-      sslCertificate = "/etc/ssl/proxy.crt";
-      sslCertificateKey = "/etc/ssl/proxy.key";
+      forceSSL = false;
       locations."/" = {
-        proxyPass = "https://192.168.11.100:3000";
+        proxyPass = "http://192.168.11.100:3000";
         proxyWebsockets = true; # allow WebSocket
       };
     };
