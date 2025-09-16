e0d4cb058f3b142ac958e2783fadeeded467ff0f
uPiscium <upiscium@gmail.com>
2025-09-16 11:46:23 +0900

    update

--- Sahaqiel/host/firewall.nix
+++ Sahaqiel/host/firewall.nix
@@ -1,7 +1,7 @@
 { ... }: {
   networking.firewall = {
-    allowedTCPPorts = [ 20000 8004 8005 11434 ];
-    allowedUDPPorts = [ 11434 ];
+    allowedTCPPorts = [ 20000 8004 8005 ];
+    allowedUDPPorts = [ ];
   };
 }
 
