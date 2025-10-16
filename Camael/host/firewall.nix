b084d39be47dc668a780b0528c156f93edf96529
uPiscium <upiscium@gmail.com>
2025-10-16 13:25:33 +0900

    update

--- Camael/host/firewall.nix
+++ Camael/host/firewall.nix
@@ -1,6 +1,6 @@
 { ... }: {
   networking.firewall = {
-    allowedTCPPorts = [ 20000 25565 ];
-    allowedTCPPortRanges = [ { from = 30000; to = 31000; } ];
+    allowedTCPPorts = [ 20000 25565 8443 8123 ];
+    allowedTCPPortRanges = [{ from = 30000; to = 31000; }];
   };
 }
