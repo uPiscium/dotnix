e88dafe696a865b5762f0a489fc517b59afa4135
uPiscium <upiscium@gmail.com>
2025-10-21 22:26:26 +0900

    update

--- Armaros/host/nginx.nix
+++ Armaros/host/nginx.nix
@@ -5,6 +5,13 @@
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
 
+    virtualHosts."yummy-control-server.upiscium.f5.si" = {
+      forceSSL = false;
+      locations."/" = {
+        proxyPass = "http://192.168.11.129:8000";
+      };
+    };
+
     virtualHosts."modelgen.upiscium.f5.si" = {
       forceSSL = false;
       locations."/" = {
