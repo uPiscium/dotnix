a25c463d168e4101a9f862b4fad53990c9717029
uPiscium <upiscium@gmail.com>
2025-11-08 22:14:03 +0900

    update

--- Cassiel/flake.nix
+++ Cassiel/flake.nix
@@ -12,7 +12,7 @@
     let
       mkNixosSystem = (import ../utils.nix { inherit inputs; }).mkNixosSystem;
       mkHomeManagerConfiguration = (import ../utils.nix { inherit inputs; }).mkHomeManagerConfiguration;
-      hostname = "Azrael";
+      hostname = "Cassiel";
       username = "upiscium";
     in
     {
--- Cassiel/host/hardware.nix
+++ Cassiel/host/hardware.nix
@@ -14,7 +14,7 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a227c2d1-db33-4cc3-8dcc-01999bd3f489";
+    { device = "/dev/disk/by-uuid/61a6c925-540d-453d-9c8b-1e7b858288ff";
       fsType = "ext4";
     };
 
