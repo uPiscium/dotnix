e48ce20b35f13b7c3c27b823ad715d47e930b36f
uPiscium <upiscium@gmail.com>
2025-11-13 10:21:50 +0900

    update

--- Cassiel/host/firewall.nix
+++ Cassiel/host/firewall.nix
@@ -1,6 +1,6 @@
 { ... }: {
   networking.firewall = {
-    allowedTCPPorts = [ 20000 11434 8000 9000 ];
+    allowedTCPPorts = [ 20000 11434 8000 8001 9000 ];
     allowedUDPPorts = [ 11434 ];
   };
 }
