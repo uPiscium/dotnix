0e830481b4cb2a71276291ee0dde9d0ad8a4bb74
uPiscium <upiscium@gmail.com>
2025-11-29 18:05:00 +0900

    update

--- Michael/host/wireguard.nix
+++ Michael/host/wireguard.nix
@@ -21,26 +21,6 @@
         }
       ];
     };
-    # wg0 = {
-    #   address = [ "10.0.0.2/32" ];
-    #   dns = [ "10.0.0.5/32" ];
-    #   mtu = 1420;
-    #   privateKeyFile = "/home/upiscium/WireGuard/Michael_.key";
-    #   postUp = ''
-    #     resolvectl dns wg0 10.0.0.5 && resolvectl domain wg0 '~.'
-    #   '';
-    #   postDown = ''
-    #     resolvectl revert wg0
-    #   '';
-    #   peers = [
-    #     {
-    #       publicKey = "rj2Fy53+fondOM1dZ9trbNaXAVEM438UAcC6ni6beQM=";
-    #       allowedIPs = [ "0.0.0.0/0" ];
-    #       endpoint = "upiscium.f5.si:30000";
-    #       persistentKeepalive = 25;
-    #     }
-    #   ];
-    # };
   };
 }
 
