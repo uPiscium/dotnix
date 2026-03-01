3ef19639b80aee86ce91094d242b25c861b304c1
uPiscium <upiscium@gmail.com>
2026-03-01 18:09:08 +0900

    update

--- Balthazar/home/packages.nix
+++ Balthazar/home/packages.nix
@@ -1,3 +1,37 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [];
+  home.packages = with pkgs; [
+    antigravity
+    bambu-studio
+    blender
+    # discord
+    vesktop
+    claude-code
+    gdlauncher-carbon
+    gpu-screen-recorder
+    gpu-screen-recorder-gtk
+    jetbrains-toolbox
+    # modrinth-app
+    # krita
+    onlyoffice-desktopeditors
+    obsidian
+    parsec-bin
+    slack
+    unityhub
+    # teams-for-linux
+    vlc
+    wireshark
+    zoom-us
+
+    dig
+    grim
+    jdk17
+    power-profiles-daemon
+    slurp
+    swappy
+    # termpdfpy
+    typst
+    unzip
+    wayland-scanner
+    yt-dlp
+  ];
 }
