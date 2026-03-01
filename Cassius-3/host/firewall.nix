b5fc7facc904b793f4d93e79a1c786242f7cd384
uPiscium <upiscium@gmail.com>
2026-03-01 16:06:26 +0900

    update

--- Cassius-3/host/firewall.nix
+++ Cassius-3/host/firewall.nix
@@ -1,6 +1,6 @@
 { ... }: {
   networking.firewall = {
-    allowedTCPPorts = [ 20000 11434 3000 8001 ];
+    allowedTCPPorts = [ 20000 11434 3000 8002 8003 ];
     allowedUDPPorts = [ 11434 3000 ];
   };
 }
