bcebb08238eb7396b7f53f7a7e07c72be063dd22
uPiscium <upiscium@gmail.com>
2026-03-02 08:20:50 +0900

    update

--- Balthazar/host/sunshine.nix
+++ Balthazar/host/sunshine.nix
@@ -1,8 +1,23 @@
-{ ... }: {
-  services.sunshine = {
-    enable = true;
-    autoStart = true;
-    capSysAdmin = true;
-    openFirewall = true;
-  };
+{ pkgs, lib, ... }: {
+#   services.sunshine.package = pkgs.sunshine.override {
+#     cudaSupport = true;
+#     cudaPackages = pkgs.cudaPackages;
+#   };
+
+#   services.avahi.publish.enable = true;
+#   services.avahi.publish.userServices = true;
+
+#   security.wrappers.sunshine = lib.mkDefault {
+#     owner = "root";
+#     group = "root";
+#     capabilities = "cap_sys_admin+p";
+#     source = "${pkgs.sunshine}/bin/sunshine";
+#   };
+
+#   services.sunshine = {
+#     enable = true;
+#     autoStart = true;
+#     capSysAdmin = true;
+#     openFirewall = true;
+#   };
 }
