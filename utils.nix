de25fa8908030b62718b3f2a67a6b61423eca1b8
uPiscium <upiscium@gmail.com>
2026-04-08 18:42:26 +0900

    Update home-manager version to 26.05

--- utils.nix
+++ utils.nix
@@ -31,7 +31,7 @@
           home = {
             inherit username;
             homeDirectory = "/home/${username}";
-            stateVersion = "25.11";
+            stateVersion = "26.05";
           };
           programs.home-manager.enable = true;
         }
