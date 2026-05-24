ccdbe8ea6823c96e0065f06bfc96f8fd3b58f287
uPiscium <upiscium@gmail.com>
2026-05-24 09:17:55 +0900

    Add Ramiel settings

---
+++ Ramiel/flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      # url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = inputs:
+    let
+      mkNixosSystem = (import ../utils.nix { inherit inputs; }).mkNixosSystem;
+      mkHomeManagerConfiguration = (import ../utils.nix { inherit inputs; }).mkHomeManagerConfiguration;
+      hostname = "Michael";
+      username = "upiscium";
+    in
+    {
+      nixosConfigurations.host = mkNixosSystem {
+        system = "x86_64-linux";
+        hostname = hostname;
+        username = username;
+        modules = [
+          ./host
+        ];
+      };
+
+      homeConfigurations.home = mkHomeManagerConfiguration {
+        system = "x86_64-linux";
+        hostname = hostname;
+        username = username;
+        modules = [
+          ./home
+        ];
+      };
+    };
+}
